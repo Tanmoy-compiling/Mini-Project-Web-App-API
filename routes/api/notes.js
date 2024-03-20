dad371633767fd72f8f37dd2bb6dcb8d567293d8
hornycompiler <Talktotanmoy.k@gmail.com>
2024-03-20 18:49:53 +0530

    v 0.2.11 add the fetch notes feature

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -5,5 +5,6 @@ const { uploadFile, uploadToS3 } = require('../../middlewares/uploadMiddleware')
 
 router.post("/insert-note", [uploadFile, uploadToS3], notesController.insertNote);
 router.delete("/delete-note", notesController.deleteNote);
+router.post("/fetch-notes", notesController.fetchNotes);
 
 module.exports = router;
