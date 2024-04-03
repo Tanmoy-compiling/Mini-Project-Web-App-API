bf4b2ef5b540c460e3b4dd7a6fb017fea545af62
hornycompiler <Talktotanmoy.k@gmail.com>
2024-04-03 16:51:09 +0530

    v0.5.0 intergrated twilio and connected with dialogflow

--- services/twilio.js
+++ services/twilio.js
@@ -1,28 +1,41 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
+const dialogflow = require("./dialogflow");
 
-module.exports.sendSMS = async function(req, res) {
-    try{
-        const message = await client.messages.create({
-        body: req.body.message,
-        from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
-        to: `whatsapp:${req.body.to}` //CLIENT'S PHONE NUMBER
-        });
-        console.log("SMS sent successfully: ", message);
-        return res.json(201, {
-        message: "SMS sent successfully",
-        data: {
-            message: message
-        }
-        });
-    }
-    catch(err){
-        console.log("******************ERROR in sending SMS: ", err);
-        return res.json(500, {
-        message: "Error in sending SMS"
-        })
-    }
-    }
+module.exports.sendSMS = async function (text, to) {
+  try {
+    const message = await client.messages.create({
+      body: text,
+      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+      to: to, //CLIENT'S PHONE NUMBER
+    });
+    console.log("SMS sent successfully: ", message.sid);
+    return { success: true, message: "SMS sent successfully", data: message };
+  } catch (err) {
+    console.log("Error in sending SMS: ", err);
+    throw new Error("Error in sending SMS"); // Throw the error to be caught by the caller
+  }
+};
 
-
+//receive message from client in twilio
+module.exports.recvMessage = async function (req, res) {
+  try {
+    const incomingMsg = req.body.Body;
+    const from = req.body.From;
+    const result = await dialogflow.processQuery(incomingMsg);
+    const sendResult = await module.exports.sendSMS(result, from);
+    if (sendResult.success) {
+      res
+        .status(200)
+        .json({ message: "Message received and response sent successfully" });
+    } else {
+      res.status(500).json({ message: "Failed to send response SMS" });
+    }
+  } catch (err) {
+    console.log("Error in receiving or responding to message: ", err);
+    res
+      .status(500)
+      .json({ message: "Error in receiving or responding to message" });
+  }
+};
